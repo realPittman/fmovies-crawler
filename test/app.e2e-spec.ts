8026b4ff09ac6fc149c47ab964574bff1ac67a4a
Hooman Hassanzadeh <mr.subscript@gmail.com>
2021-06-17 18:01:38 +0430

    improved e2e tests

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -21,15 +21,86 @@ describe('AppController (e2e)', () => {
     await app.getHttpAdapter().getInstance().ready();
   });
 
-  it(`/ GET`, async () => {
-    app
-      .inject({
+  it(`GET /`, async () => {
+    const result = await app.inject({
+      method: 'GET',
+      url: '/',
+    });
+    expect(result.statusCode).toEqual(404);
+  });
+
+  describe('Home', () => {
+    it(`GET /home`, async () => {
+      const result = await app.inject({
         method: 'GET',
-        url: '/',
-      })
-      .then((result) => {
-        expect(result.statusCode).toEqual(404);
+        url: '/home',
       });
+      expect(result.statusCode).toEqual(200);
+      // TODO: check response
+    });
+  });
+
+  describe('Search', () => {
+    it(`GET /search/available-options`, async () => {
+      const result = await app.inject({
+        method: 'GET',
+        url: '/search/available-options',
+      });
+      expect(result.statusCode).toEqual(200);
+      // TODO: check response
+    });
+
+    describe('Simple', () => {
+      it(`GET /search/simple`, async () => {
+        const result = await app.inject({
+          method: 'GET',
+          url: '/search/simple',
+          query: {
+            keyword: 'test',
+          },
+        });
+        expect(result.statusCode).toEqual(200);
+        // TODO: check response
+      });
+
+      // TODO: add two more simple tests
+    });
+
+    describe('Advanced', () => {
+      it(`GET /search/advanced`, async () => {
+        const result = await app.inject({
+          method: 'GET',
+          url: '/search/advanced',
+          // TODO: add params
+        });
+        expect(result.statusCode).toEqual(200);
+        // TODO: check response
+      });
+
+      // TODO: add more advanced searches
+    });
+  });
+
+  describe('Video', () => {
+    describe('By ID', () => {
+      it(`GET /video`, async () => {
+        const result = await app.inject({
+          method: 'GET',
+          url: '/video',
+          query: {
+            id: 'qnowv', // The flash
+          },
+        });
+        expect(result.statusCode).toEqual(200);
+        // TODO: check response
+      });
+
+      // TODO: add more find by id requests
+    });
+
+    describe('Details', () => {
+      // TODO: write logic
+    });
   });
 
   afterAll(async () => {
