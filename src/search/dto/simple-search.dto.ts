1537032bc43632c4e36c219bc6614866f11334be
Hooman Hassanzadeh <mr.subscript@gmail.com>
2021-06-17 18:01:23 +0430

    added IsDefined decorator to keyword

--- src/search/dto/simple-search.dto.ts
+++ src/search/dto/simple-search.dto.ts
@@ -1,9 +1,10 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsDefined, IsNotEmpty, IsString } from 'class-validator';
 
 export class SimpleSearchDto {
   @IsString()
   @IsNotEmpty()
+  @IsDefined()
   @ApiProperty({ description: 'The keyword query to search' })
   keyword: string;
 }
